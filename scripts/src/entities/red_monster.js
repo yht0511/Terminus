5eb5a24a0d14474bebe68223026019e99b8f4886
cchen <1120243698@bit.edu.cn>
2025-09-10 17:21:34 +0800

    追逐语音

--- scripts/src/entities/red_monster.js
+++ scripts/src/entities/red_monster.js
@@ -180,7 +180,7 @@ export default class RedMonster {
         // 当前距离
         const distance = model.position.distanceTo(target);
         // 步长（可调整速度）
-        const step = Math.min(0.2, distance);
+        const step = Math.min(0.12, distance);
 
         // 根据移动方向调整角度
         if (distance > 0.01) {
